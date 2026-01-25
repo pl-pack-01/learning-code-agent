86adf4cf888081e7dc5ae7f9865d85f1cc63727f
Michael Pack <michaelraypack@gmail.com>
2026-01-25 15:39:11 -0500

    Finish LLM section.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os, argparse
 from urllib import response
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
     load_dotenv()
@@ -9,16 +10,20 @@ def main():
 
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
 
     if api_key is None:
         raise ValueError("GEMINI_API_KEY not found in environment variables.")
     else:
         client = genai.Client(api_key=api_key)
+        messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
         response = client.models.generate_content(
-            model='gemini-2.5-flash', contents=args.user_prompt)
-        print("Prompt tokens: ", response.usage_metadata.prompt_token_count)
-        print("Response tokens: ", response.usage_metadata.candidates_token_count)
+            model='gemini-2.5-flash', contents=messages)
+        if args.verbose:
+            print("User prompt: ", args.user_prompt)
+            print("Prompt tokens: ", response.usage_metadata.prompt_token_count)
+            print("Response tokens: ", response.usage_metadata.candidates_token_count)
 
         print(response.text)
 
